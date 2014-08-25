9103ab51a3e11d8efb586ae0c4e2381e103539bb
Derek Willis <dwillis@gmail.com>
2014-08-24 22:18:29 -0400

    added template

--- linkchecker.py
+++ linkchecker.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 application = Flask(__name__)
 application.debug = True
 application.config['PORT'] = 5000
 
 @application.route('/', methods=['GET'])
-def landing():
-    return "Linkchecker!"
+def index():
+    return render_template('index.html')
+
