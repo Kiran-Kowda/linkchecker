81f552876ebd7193b1ccf9be50fb23b8d2f84522
palewire <ben.welsh@gmail.com>
2014-08-25 08:53:07 -0700

    Little HTML cleanup that fixes some broken tags and bad links

--- linkchecker.py
+++ linkchecker.py
@@ -5,13 +5,19 @@ application = Flask(__name__)
 application.debug = True
 application.config['PORT'] = 5000
 
+
 @application.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+
 @application.route('/result/', methods=['POST', 'GET'])
 def result():
-	u = request.form['url']
-	url = storytracker.archive(u)
-	links = [h for h in url.hyperlinks if 'wikipedia' in h.href]
-	return render_template('results.html', links=links, num_links = len(links), url=u)
+    u = request.form['url']
+    url = storytracker.archive(u)
+    links = [h for h in url.hyperlinks if 'wikipedia' in h.href]
+    return render_template('results.html', links=links, num_links = len(links), url=u)
+
+
+if __name__ == "__main__":
+    application.run()
