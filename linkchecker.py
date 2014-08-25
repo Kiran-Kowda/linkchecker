61e3e8c7c0e1cc47e5d06915a95cde40ba87a0a9
Derek Willis <dwillis@gmail.com>
2014-08-25 07:24:14 -0400

    added result

--- linkchecker.py
+++ linkchecker.py
@@ -10,10 +10,9 @@ application.config['PORT'] = 5000
 def index():
     return render_template('index.html')
 
-
-@application.route('/result', methods=['POST'])
+@application.route('/result/', methods=['POST', 'GET'])
 def result():
 	u = request.form['url']
 	url = storytracker.archive(u)
 	links = [h for h in url.hyperlinks if 'wikipedia' in h.href]
-	return render_template('index.html', links=links)
+	return render_template('results.html', links=links, num_links = len(links), url=u)
